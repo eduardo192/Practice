5977e53f56b14487e947905884495e44dc07a380
Eduardo Anguiano <marcos.2000.mg15@gmail.com>
2022-07-29 14:55:37 -0500

    Creacion del modleo para conectar a la base de datos

---
+++ app/Models/MovieModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MovieModel extends Model
+{
+    protected $table = 'movies';
+    protected $primaryKey = 'id';
+
+    public function get($id = null)
+    {
+        if ($id === null) {
+            return $this->findAll();
+        }
+
+        return $this->asArray()->where(['id' => $id])->first();
+    }
+
+}
