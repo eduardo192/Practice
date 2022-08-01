b598cab12141ac89d0ef6ab6010d2b1c8e25e357
Eduardo Anguiano <marcos.2000.mg15@gmail.com>
2022-08-01 14:34:30 -0500

    recibir por formularios por post

---
+++ app/Views/dashboard/movies/new.php
@@ -0,0 +1,10 @@
+<form action="post" method="POST">
+
+    <label for="title">Title</label>
+    <input type="input" id="title" name="title" /><br />
+
+    <label for="description">Text</label>
+    <textarea name="description" id="description" cols="45" rows="4"></textarea><br />
+
+    <input type="submit" name="submit" value="Guardar" />
+</form>
