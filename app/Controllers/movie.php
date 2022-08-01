cbedb2d87be1a34d41aa826d0c8e7bde9bb7de87
Eduardo Anguiano <marcos.2000.mg15@gmail.com>
2022-08-01 12:40:56 -0500

    Creacion de funcion new

--- app/Controllers/movie.php
+++ app/Controllers/movie.php
@@ -3,17 +3,10 @@
 // este use se coloca para que podamos alcanzar el Base Controller desde la capreta en la que estamos.
 use App\Models\MovieModel;
 
-class movie extends BaseController{
+class Movie extends BaseController{
     public function index(){
 
         $movie = new MovieModel();
-        
-        //array que contine los valores que se van a madra por parametro a las vistas
-        $dataHeader = [
-            //este dato se pude acceder desde la vista usando la clave del array como una variable php
-            'title' => 'Listado de Peliculas',
-            'title2' => 'Listado de pelicuals 2'
-        ];
 
         $data = [
             /* 
@@ -27,33 +20,31 @@ class movie extends BaseController{
         ];
 
         //los datos que se quieren mandar hacia las vistas se ponen como segundo paramatro en la vista y debe ser un array 
-        echo view("dashboard/templates/header",$dataHeader);
-        echo view("dashboard/movies/index",$data);
-        echo view("dashboard/templates/footer");
+        $this->_loadDefaulView('Listado de Peliculas',$data,'index');
     }
 
-    public function test($name = "andres"){
-        //array que contine los valores que se van a madra por parametro a las vistas
-        $dataHeader = [
-            //este dato se pude acceder desde la vista usando la clave del array como una variable php
-            'title' => 'Listado de Peliculas '.$name
-           
-        ];
-
-        $data = [
-            'movies' => array(0,1,2,3,4)
-
-        ];
-
-        //los datos que se quieren mandar hacia las vistas se ponen como segundo paramatro en la vista y debe ser un array 
-        echo view("dashboard/templates/header",$dataHeader);
-        echo view("dashboard/movies/index",$data);
-        echo view("dashboard/templates/footer");
+    public function new(){
+        $movie = new MovieModel();
+        
+        $this->_loadDefaulView('Crear una Pelicula', [], 'new');
+        
     }
+
 
     public function show ($id = null){
         $movie = new MovieModel();
 
         var_dump($movie->get($id));
+    }
+
+    private function _loadDefaulView($title,$data,$view){
+        $dataHeader = [
+            //este dato se pude acceder desde la vista usando la clave del array como una variable php
+            'title' => $title
+        ];
+
+        echo view("dashboard/templates/header",$dataHeader);
+        echo view("dashboard/movies/$view",$data);
+        echo view("dashboard/templates/footer");
     }
 }
