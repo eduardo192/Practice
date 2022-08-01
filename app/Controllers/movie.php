f8453a7c2b75cb685b3406a028c1ea825e2af143
Eduardo Anguiano <marcos.2000.mg15@gmail.com>
2022-07-31 19:37:27 -0500

    Configuracion del controlaodr para que funcione con el metodo resorces

---
+++ app/Controllers/movie.php
@@ -0,0 +1,59 @@
+<?php namespace App\Controllers;
+
+// este use se coloca para que podamos alcanzar el Base Controller desde la capreta en la que estamos.
+use App\Models\MovieModel;
+
+class movie extends BaseController{
+    public function index(){
+
+        $movie = new MovieModel();
+        
+        //array que contine los valores que se van a madra por parametro a las vistas
+        $dataHeader = [
+            //este dato se pude acceder desde la vista usando la clave del array como una variable php
+            'title' => 'Listado de Peliculas',
+            'title2' => 'Listado de pelicuals 2'
+        ];
+
+        $data = [
+            /* 
+                paginate nos ayuda a hacer la paginacion.
+                El primer parametro indica la cantidad de registros que queremos traer a la vez.
+            */
+            'movies' => $movie->asObject()->paginate(5),
+            //instncia para poder usar el pager link
+            'pager' => $movie->pager
+
+        ];
+
+        //los datos que se quieren mandar hacia las vistas se ponen como segundo paramatro en la vista y debe ser un array 
+        echo view("dashboard/templates/header",$dataHeader);
+        echo view("dashboard/movies/index",$data);
+        echo view("dashboard/templates/footer");
+    }
+
+    public function test($name = "andres"){
+        //array que contine los valores que se van a madra por parametro a las vistas
+        $dataHeader = [
+            //este dato se pude acceder desde la vista usando la clave del array como una variable php
+            'title' => 'Listado de Peliculas '.$name
+           
+        ];
+
+        $data = [
+            'movies' => array(0,1,2,3,4)
+
+        ];
+
+        //los datos que se quieren mandar hacia las vistas se ponen como segundo paramatro en la vista y debe ser un array 
+        echo view("dashboard/templates/header",$dataHeader);
+        echo view("dashboard/movies/index",$data);
+        echo view("dashboard/templates/footer");
+    }
+
+    public function show ($id = null){
+        $movie = new MovieModel();
+
+        var_dump($movie->get($id));
+    }
+}
